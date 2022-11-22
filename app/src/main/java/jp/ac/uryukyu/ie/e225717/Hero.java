f95c65318bdfa6faeb6ad5960e0cea26a552e499
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-11-22 09:59:05 +0900

    コメント修正

--- app/src/main/java/jp/ac/uryukyu/ie/e225717/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225717/Hero.java
@@ -2,10 +2,10 @@ package jp.ac.uryukyu.ie.e225717;
 
 /**
  * ヒーロークラス。
- * String name; //敵の名前
- * int hitPoint; //敵のHP
- * int attack; //敵の攻撃力
- * boolean dead; //敵の生死状態。true=死亡。
+ * String name; //名前
+ * int hitPoint; //HP
+ * int attack; //攻撃力
+ * boolean dead; //生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
 public class Hero extends LivingThing {
